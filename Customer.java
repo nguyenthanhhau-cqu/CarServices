2d2ab16c2089a695a821e382337d65135aef9510
Hoan130901 <62161315+Hoan130901@users.noreply.github.com>
2021-06-13 22:20:19 +1000

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment3sofware.Model;
+
+/**
+ *
+ * @author andre
+ */
+public class Customer {
+    private int customerID;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String address;
+    
+    public Customer() {
+        
+    }
+
+    public Customer(int customerID, String firstName, String lastName, String phone, String address) {
+        this.customerID = customerID;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.address = address;
+        this.firstName = firstName;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    
+}
